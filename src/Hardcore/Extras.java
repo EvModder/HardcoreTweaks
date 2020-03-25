bb35d2ee8fad0d9b8194a5672d559019cee617e9
EvModder <evmodder@gmail.com>
2020-03-25 03:57:34 -0700

    Added filter to remove illegit horses from earlier glitch

--- src/Hardcore/Extras.java
+++ src/Hardcore/Extras.java
@@ -1,9 +1,13 @@
 package Hardcore;
 
 import org.bukkit.GameMode;
+import org.bukkit.entity.AbstractHorse;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.world.ChunkLoadEvent;
+import net.evmodder.HorseOwners.HorseLibrary;
 
 class Extras implements Listener{
 	Extras(){
@@ -19,6 +23,20 @@ class Extras implements Listener{
 
 		if(command.contains("suicide") && !evt.getPlayer().isOp() && evt.getPlayer().getGameMode() == GameMode.SPECTATOR){
 			evt.setCancelled(true);
+		}
+	}
+	@EventHandler
+	public void onChunkLoad(ChunkLoadEvent evt){
+		if(evt.isNewChunk() || evt.isAsynchronous()) return;
+		for(Entity e : evt.getChunk().getEntities()){
+			if(e instanceof AbstractHorse){
+				AbstractHorse horse = (AbstractHorse)e;
+				double speed = HorseLibrary.getNormalSpeed(horse), jump = HorseLibrary.getNormalJump(horse), health = HorseLibrary.getNormalMaxHealth(horse);
+				if(speed > 16 || jump > 6 || health > 35){
+					horse.remove();
+					HCTweaks.getPlugin().getLogger().warning("Deleted illegit horse: s,j,h="+speed+","+jump+","+health);
+				}
+			}
 		}
 	}
 }
