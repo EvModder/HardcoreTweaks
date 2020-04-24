74096390e1a60ef899159c4bea234483aaa33953
EvModder <evmodder@gmail.com>
2020-04-24 01:29:39 -0700

    Change spawn loc generation and use barriers instead of bedrock

--- src/Hardcore/NewPlayerManager.java
+++ src/Hardcore/NewPlayerManager.java
@@ -18,6 +18,7 @@ import org.bukkit.attribute.Attribute;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.Container;
+import org.bukkit.block.EndGateway;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -99,6 +100,11 @@ public class NewPlayerManager implements Listener{
 		}
 		return false;
 	}
+	boolean isOverLiquid(Location loc){
+		Block b = loc.getBlock();
+		while(b != null && b.isEmpty() && b.isPassable() && !b.isLiquid()) b = b.getRelative(BlockFace.DOWN);
+		return b.isLiquid();
+	}
 	double getRandomCoord(double maxCoord, double scale, double randDouble){
 		//Into wolfram: (e^(p*11.58 - 0.57721566490) - 0.5)*500
 		pl.getLogger().info("Generating random coord, scale="+scale);
@@ -140,74 +146,56 @@ public class NewPlayerManager implements Listener{
 				return null;
 			}
 
-			while(loc.getBlockY() > seaLevel & (loc.getBlock() == null || loc.getBlock().isEmpty() 
-					|| loc.getBlock().isPassable())) loc.setY(loc.getY() - 1);
-			loc.setY(loc.getY() + 2);
+			while(loc.getBlockY() > seaLevel && (loc.getBlock() == null || loc.getBlock().isEmpty())) loc.setY(loc.getY() - 1);
 			String debugStr = "Candidate X,Y,Z: "+loc.getBlockX()+" "+loc.getBlockY()+" "+loc.getBlockZ();
-			if(loc.getY() < seaLevel + 3)
-				pl.getLogger().info(debugStr+" >> Below sea level");
-			else if(loc.getBlock().getRelative(BlockFace.DOWN).isLiquid())
-				pl.getLogger().info(debugStr+" >> Over liquid");
-			else if(hasNearbyLava(loc))
-				pl.getLogger().info(debugStr+" >> Near to lava");
+
+			if(loc.getY() < seaLevel) pl.getLogger().info(debugStr+" >> Below sea level (Y="+loc.getBlockY()+")");
+			else if(isOverLiquid(loc)) pl.getLogger().info(debugStr+" >> Over liquid");
+			else if(hasNearbyLava(loc)) pl.getLogger().info(debugStr+" >> Near to lava");
 			else{
 				pl.getLogger().info(debugStr+" >> SUCCESS");
+				loc.setY(loc.getY() + 3); // Two blocks above the ground
 				return loc;
 			}
 		}
 	}
 
-	static void removeNearbyBedrock(Location loc){
+	static void removeSpawnBox(Location loc){
 		for(int x=-6; x<=6; ++x) for(int y=-6; y<=6; ++y) for(int z=-6; z<=6; ++z){
 			Block block = loc.clone().add(x, y, z).getBlock();
-			if(block != null && block.getType() == Material.BEDROCK) block.setType(Material.AIR);
+			if(block != null && block.getType() == Material.BARRIER || block.getType() == Material.END_GATEWAY) block.setType(Material.AIR);
 		}
 	}
 
-	void createSpawnBox(Location loc){
+	void createSpawnBox(Location loc, Player player){
 		// Always set the 6 faces
-		loc.getBlock().getRelative(BlockFace.UP).setType(Material.BEDROCK);
-		loc.getBlock().getRelative(BlockFace.DOWN).setType(Material.BEDROCK);
-		loc.getBlock().getRelative(BlockFace.NORTH).setType(Material.BEDROCK);
-		loc.getBlock().getRelative(BlockFace.SOUTH).setType(Material.BEDROCK);
-		loc.getBlock().getRelative(BlockFace.EAST).setType(Material.BEDROCK);
-		loc.getBlock().getRelative(BlockFace.WEST).setType(Material.BEDROCK);
+		loc.getBlock().getRelative(BlockFace.UP).setType(Material.BARRIER);
+		loc.getBlock().getRelative(BlockFace.DOWN).setType(Material.BARRIER);
+		loc.getBlock().getRelative(BlockFace.NORTH).setType(Material.BARRIER);
+		loc.getBlock().getRelative(BlockFace.SOUTH).setType(Material.BARRIER);
+		loc.getBlock().getRelative(BlockFace.EAST).setType(Material.BARRIER);
+		loc.getBlock().getRelative(BlockFace.WEST).setType(Material.BARRIER);
 		for(int x=-2; x<=2; ++x) for(int y=-2; y<=2; ++y) for(int z=-2; z<=2; ++z){
 			Block block = loc.clone().add(x, y, z).getBlock();
-			if(block.isEmpty()) block.setType(Material.BEDROCK);
+			if(block.isEmpty()) block.setType(Material.BARRIER);
 		}
 		loc.getBlock().setType(Material.AIR);
-	}
 
-	void sendSpawnBoxAtmosphere(Player player){
-		player.sendBlockChange(player.getLocation().add(0, 1, 0), Material.BARRIER.createBlockData());
-		player.sendBlockChange(player.getLocation().add(0, -1, 0), Material.BARRIER.createBlockData());
-		player.sendBlockChange(player.getLocation().add(1, 0, 0), Material.BARRIER.createBlockData());
-		player.sendBlockChange(player.getLocation().add(-1, 0, 0), Material.BARRIER.createBlockData());
-		player.sendBlockChange(player.getLocation().add(0, 0, 1), Material.BARRIER.createBlockData());
-		player.sendBlockChange(player.getLocation().add(0, 0, -1), Material.BARRIER.createBlockData());
-
-		player.sendBlockChange(player.getLocation(), Material.END_GATEWAY.createBlockData());
-		BlockData blockData = Material.END_GATEWAY.createBlockData();
-		//blockData.setAge(-1000000000);
-		player.sendBlockChange(player.getLocation().add(0, 2, 0), blockData);
-		player.sendBlockChange(player.getLocation().add(0, -2, 0), blockData);
-		player.sendBlockChange(player.getLocation().add(2, 0, 0), blockData);
-		player.sendBlockChange(player.getLocation().add(-2, 0, 0), blockData);
-		player.sendBlockChange(player.getLocation().add(0, 0, 2), blockData);
-		player.sendBlockChange(player.getLocation().add(0, 0, -2), blockData);
-		player.sendBlockChange(player.getLocation().add(1, 1, 0), blockData);
-		player.sendBlockChange(player.getLocation().add(-1, 1, 0), blockData);
-		player.sendBlockChange(player.getLocation().add(0, 1, 1), blockData);
-		player.sendBlockChange(player.getLocation().add(0, 1, -1), blockData);
-		player.sendBlockChange(player.getLocation().add(1, -1, 0), blockData);
-		player.sendBlockChange(player.getLocation().add(-1, -1, 0), blockData);
-		player.sendBlockChange(player.getLocation().add(0, -1, 1), blockData);
-		player.sendBlockChange(player.getLocation().add(0, -1, -1), blockData);
-		player.sendBlockChange(player.getLocation().add(1, 0, 1), blockData);
-		player.sendBlockChange(player.getLocation().add(-1, 0, 1), blockData);
-		player.sendBlockChange(player.getLocation().add(1, 0, -1), blockData);
-		player.sendBlockChange(player.getLocation().add(-1, 0, -1), blockData);
+		int[] xs = new int[]{2,0,0, -2,-0,-0,  1,1,0, -1,-1,-0,  +1,-1,+1,-1,+0,+0};
+		int[] ys = new int[]{0,2,0, -0,-2,-0,  1,0,1, -1,-0,-1,  -1,+1,+0,+0,+1,-1};
+		int[] zs = new int[]{0,0,2, -0,-0,-2,  0,1,1, -0,-1,-1,  +0,+0,-1,+1,-1,+1};
+		for(int i=0; i<xs.length; ++i){
+			Block block = loc.clone().add(xs[i], ys[i], zs[i]).getBlock();
+			if(block.isEmpty()){
+				block.setType(Material.END_GATEWAY);
+				EndGateway gatewayState = (EndGateway)block.getState();
+				gatewayState.setAge(-1000000000);
+			}
+			else if(player != null){
+				BlockData blockData = Material.END_GATEWAY.createBlockData();
+				player.sendBlockChange(player.getLocation().add(0, 2, 0), blockData);
+			}
+		}
 	}
 
 	public void giveGuideBook(Player player){
@@ -247,8 +235,7 @@ public class NewPlayerManager implements Listener{
 			saveSpawnLocs();
 		}}.runTaskLater/*Asynchronously*/(pl, 20*60);//60s
 
-		createSpawnBox(spawnLoc);
-		sendSpawnBoxAtmosphere(player);
+		createSpawnBox(spawnLoc, player);
 
 		spawnLoc.setX(spawnLoc.getBlockX() + 0.5);
 		spawnLoc.setZ(spawnLoc.getBlockZ() + 0.5);
@@ -257,7 +244,7 @@ public class NewPlayerManager implements Listener{
 			Player player = pl.getServer().getPlayer(uuid);
 			if(player != null){
 				player.teleport(spawnLoc);
-				sendSpawnBoxAtmosphere(player);
+				createSpawnBox(spawnLoc, player);
 			}
 		}}.runTaskLater(pl, 20);
 		player.setBedSpawnLocation(spawnLoc);
@@ -310,11 +297,10 @@ public class NewPlayerManager implements Listener{
 		if(player.getScoreboardTags().contains("unconfirmed")){
 			player.sendMessage(ChatColor.GREEN+">> "
 					+ChatColor.GOLD+ChatColor.BOLD+"Read the book to get started");
-			createSpawnBox(player.getLocation());
-			sendSpawnBoxAtmosphere(player);
-			new BukkitRunnable(){@Override public void run(){sendSpawnBoxAtmosphere(player);}}.runTaskLater(pl, 1*20);
-			new BukkitRunnable(){@Override public void run(){sendSpawnBoxAtmosphere(player);}}.runTaskLater(pl, 2*20);
-			new BukkitRunnable(){@Override public void run(){sendSpawnBoxAtmosphere(player);}}.runTaskLater(pl, 4*20);
+			createSpawnBox(player.getLocation(), player);
+			new BukkitRunnable(){@Override public void run(){createSpawnBox(player.getLocation(), player);}}.runTaskLater(pl, 1*20);
+			new BukkitRunnable(){@Override public void run(){createSpawnBox(player.getLocation(), player);}}.runTaskLater(pl, 2*20);
+			new BukkitRunnable(){@Override public void run(){createSpawnBox(player.getLocation(), player);}}.runTaskLater(pl, 4*20);
 			player.getInventory().clear();
 			giveGuideBook(player);
 		}
@@ -338,7 +324,7 @@ public class NewPlayerManager implements Listener{
 	@EventHandler
 	public void onPlayerQuit(PlayerQuitEvent evt){
 		if(evt.getPlayer().getScoreboardTags().contains("unconfirmed")){
-			removeNearbyBedrock(evt.getPlayer().getLocation());
+			removeSpawnBox(evt.getPlayer().getLocation());
 		}
 	}
 
@@ -348,7 +334,7 @@ public class NewPlayerManager implements Listener{
 			Player player = evt.getPlayer();
 			evt.setCancelled(true);
 			player.setWalkSpeed(0.2f);
-			NewPlayerManager.removeNearbyBedrock(player.getLocation());
+			removeSpawnBox(player.getLocation());
 			player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 10, 3), true);
 			player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 10, 3), true);
 			player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 2, 0), true);
